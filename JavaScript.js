317757374497befc0fd6b64de3df39e1d08523d5
Kealiwyn <67714527+Kealiwyn@users.noreply.github.com>
2021-03-22 11:19:24 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,56 @@
+﻿function brm1() {
+    var bmr;
+    var Gender;
+    var calories = 0;
+    var Age = document.getElementById("age").value;
+    var Height = document.getElementById("height").value;
+    var Weight = document.getElementById("weight").value;
+    var Gender = document.getElementById("Gender").value;
+
+    var activityLevel1 = document.getElementById("activityLevel1");
+    var activityLevel2 = document.getElementById("activityLevel2");
+    var activityLevel3 = document.getElementById("activityLevel3");
+    var activityLevel4 = document.getElementById("activityLevel4");
+    var activityLevel5 = document.getElementById("activityLevel5");
+    (activityLevel1.checked) ? (calories += 1.2) : (calories += 0);
+    (activityLevel2.checked) ? (calories += 1.375) : (calories += 0);
+    (activityLevel3.checked) ? (calories += 1.55) : (calories += 0);
+    (activityLevel4.checked) ? (calories += 1.725) : (calories += 0);
+    (activityLevel5.checked) ? (calories += 1.9) : (calories += 0);
+    (Gender == "Male") ? bmr = 66.5 + (13.75 * Weight) + (5.003 * Height) - (6.755 * Age) : bmr = 655 + (9.563 * Weight) + (1.850 * Height) - (4.676 * Age);
+    (Gender == "Female") ? bmr = 655 + (9.563 * Weight) + (1.850 * Height) - (4.676 * Age) : bmr = 66.5 + (13.75 * Weight) + (5.003 * Height) - (6.755 * Age);
+    GrandCalories = bmr + calories;
+
+    document.getElementById("bmr").innerHTML = bmr.toFixed(2);
+    document.getElementById("calories").innerHTML = GrandCalories.toFixed(2);
+    document.getElementById("gender").innerHTML = Gender;
+}
+
+
+function bmr2() {
+    var bmr;
+    var Gender;
+    var calories = 0;
+    var Age = document.getElementById("age").value;
+    var Height = document.getElementById("height").value;
+    var Weight = document.getElementById("weight").value;
+    var Gender = document.getElementById("Gender").value;
+
+    var activityLevel1 = document.getElementById("activityLevel1");
+    var activityLevel2 = document.getElementById("activityLevel2");
+    var activityLevel3 = document.getElementById("activityLevel3");
+    var activityLevel4 = document.getElementById("activityLevel4");
+    var activityLevel5 = document.getElementById("activityLevel5");
+    (activityLevel1.checked) ? (calories += 1.2) : (calories += 0);
+    (activityLevel2.checked) ? (calories += 1.375) : (calories += 0);
+    (activityLevel3.checked) ? (calories += 1.55) : (calories += 0);
+    (activityLevel4.checked) ? (calories += 1.725) : (calories += 0);
+    (activityLevel5.checked) ? (calories += 1.9) : (calories += 0);
+    (Gender == "Male") ? bmr = 66 + (6.2 * Weight) + (12.7 * Height) - (6.76 * Age) : 655 + (4.35 * Weight) + (4.7 * Height) - (4.7 * Age);
+    (Gender == "Female") ? bmr = 655 + (4.35 * Weight) + (4.7 * Height) - (4.7 * Age) : 66 + (6.2 * Weight) + (12.7 * Height) - (6.76 * Age);
+    GrandCalories = bmr + calories;
+
+    document.getElementById("bmr").innerHTML = bmr.toFixed(2);
+    document.getElementById("calories").innerHTML = GrandCalories.toFixed(2);
+    document.getElementById("gender").innerHTML = Gender;
+}
